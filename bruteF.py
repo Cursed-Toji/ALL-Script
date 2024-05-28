2319b45ce9c9600c045af457d4d3238d1e02357b
Cals <88802566+victorcals@users.noreply.github.com>
2024-05-28 03:34:22 -0300

    Update bruteF.py

--- bruteF.py
+++ bruteF.py
@@ -2,13 +2,14 @@
 
 import requests
 import sys
+import argparse
 from urllib.parse import urljoin
 
-def brute(url, wordlist_file):
-    with open(wordlist_file, 'r') as file:
-        wordlist = file.readlines()
-
+def brute_directories(url, wordlist_file):
     try:
+        with open(wordlist_file, 'r') as file:
+            wordlist = file.readlines()
+
         for word in wordlist:
             word = word.strip()
             urlfinal = urljoin(url, word)
@@ -25,13 +26,44 @@ def brute(url, wordlist_file):
     except:
         pass
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Uso: python3 script.py <url> <arquivo_de_lista_de_palavras>")
+def brute_subdomains(domain, wordlist_file):
+    try:
+        with open(wordlist_file, 'r') as file:
+            wordlist = file.readlines()
+
+        for word in wordlist:
+            word = word.strip()
+            subdomain = '{}.{}'.format(word, domain)
+            try:
+                response = requests.get('http://{}'.format(subdomain))
+                code = response.status_code
+
+                if code != 404:
+                    print('{}: {} {}'.format(subdomain, domain, code))
+            except requests.exceptions.RequestException as e:
+                print("Erro ao conectar-se à URL:", e)
+    except KeyboardInterrupt:
+        sys.exit('User cancelled the program.')
+    except:
+        pass
+
+def main():
+    parser = argparse.ArgumentParser(description="Brute force directories or subdomains.")
+    parser.add_argument("-d", "--directories", action="store_true", help="Brute force directories")
+    parser.add_argument("-s", "--subdomains", action="store_true", help="Brute force subdomains")
+    parser.add_argument("target", help="Target URL or domain")
+    parser.add_argument("wordlist", help="Wordlist file")
+
+    args = parser.parse_args()
+
+    if args.directories:
+        brute_directories(args.target, args.wordlist)
+    elif args.subdomains:
+        brute_subdomains(args.target, args.wordlist)
+    else:
+        print("Please specify either -d or -s option.")
+        parser.print_help()
         sys.exit(1)
 
-    url = sys.argv[1]
-    wordlist_file = sys.argv[2]
-
-    brute(url, wordlist_file)
-
+if __name__ == '__main__':
+    main()
